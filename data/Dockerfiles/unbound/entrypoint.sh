8f8ea59d3cae4d8c37fd8fb428b0b080a8044b2c
Georg Pfuetzenreuter <mail@georg-pfuetzenreuter.net>
2022-12-04 18:14:28 +0100

    Patch Unbound configuration
    Signed-off-by: Georg Pfuetzenreuter <mail@georg-pfuetzenreuter.net>

--- data/Dockerfiles/unbound/entrypoint.sh
+++ data/Dockerfiles/unbound/entrypoint.sh
@@ -2,12 +2,27 @@
 set -Ceu
 
 printf 'Init 1 ..\n'
-/usr/sbin/unbound-anchor -v
+/usr/sbin/unbound-anchor -a /var/lib/unbound/root.key -v
+mv /var/lib/unbound/root.key /etc/unbound/root.key
 
 printf 'Init 2 ..\n'
 /usr/sbin/unbound-control-setup 2>/dev/null
 
 printf 'Init 3 ..\n'
+hintsfile='/etc/unbound/root.hints'
+if [ ! -f "$hintsfile" ]
+then
+	curl -sSo "$hintsfile" https://www.internic.net/domain/named.root
+fi
+
+printf 'Init 4 ..\n'
+rundir='/run/unbound'
+if [ ! -d "$rundir" ]
+then
+	mkdir "$rundir"
+fi
+
+printf 'Init 5 ..\n'
 for file in /hooks/*; do
   if [ -x "${file}" ]; then
     echo "Running hook ${file}"
